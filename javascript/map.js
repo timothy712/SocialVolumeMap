52a0e45ed7c31fce9c79e4df6243408396230e3e
timohy <timothy820712@gmail.com>
2017-08-21 11:33:40 +0800

    change

---
+++ javascript/map.js
@@ -0,0 +1,96 @@
+
+function showInfoNanzhih() {
+   document.getElementById('showboxNanzhih').style.display = "block";
+}
+
+function hideInfoNanzhih() {
+   document.getElementById('showboxNanzhih').style.display = "none";
+}
+
+function showInfoZuoying() {
+   document.getElementById('showboxZuoying').style.display = "block";
+}
+
+function hideInfoZuoying() {
+   document.getElementById('showboxZuoying').style.display = "none";
+}
+
+function showInfoYenchao() {
+   document.getElementById('showboxYenchao').style.display = "block";
+}
+
+function hideInfoYenchao() {
+   document.getElementById('showboxYenchao').style.display = "none";
+}
+
+function showInfoKangshan() {
+   document.getElementById('showboxKangshan').style.display = "block";
+}
+
+function hideInfoKangshan() {
+   document.getElementById('showboxKangshan').style.display = "none";
+}
+
+function showInfoDashe() {
+   document.getElementById('showboxDashe').style.display = "block";
+}
+
+function hideInfoDashe() {
+   document.getElementById('showboxDashe').style.display = "none";
+}
+
+function showInfoKushan() {
+   document.getElementById('showboxKushan').style.display = "block";
+}
+
+function hideInfoKushan() {
+   document.getElementById('showboxKushan').style.display = "none";
+}
+
+function showInfoSanming() {
+   document.getElementById('showboxSanming').style.display = "block";
+}
+
+function hideInfoSanming() {
+   document.getElementById('showboxSanming').style.display = "none";
+}
+
+function showInfoRenwu() {
+   document.getElementById('showboxRenwu').style.display = "block";
+}
+
+function hideInfoRenwu() {
+   document.getElementById('showboxRenwu').style.display = "none";
+}
+
+function showInfoCiaotou() {
+   document.getElementById('showboxCiaotou').style.display = "block";
+}
+
+function hideInfoCiaotou() {
+   document.getElementById('showboxCiaotou').style.display = "none";
+}
+
+function showInfoZiguan() {
+   document.getElementById('showboxZiguan').style.display = "block";
+}
+
+function hideInfoZiguan() {
+   document.getElementById('showboxZiguan').style.display = "none";
+}
+
+function showInfoMituo() {
+   document.getElementById('showboxMituo').style.display = "block";
+}
+
+function hideInfoMituo() {
+   document.getElementById('showboxMituo').style.display = "none";
+}
+
+function showInfoYongan() {
+   document.getElementById('showboxYongan').style.display = "block";
+}
+
+function hideInfoYongan() {
+   document.getElementById('showboxYongan').style.display = "none";
+}
